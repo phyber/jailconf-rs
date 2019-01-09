e84c7bc3be70bec3823b0661a72dd058108efb6f
David O'Rourke <david.orourke@gmail.com>
2019-01-09 09:15:05 +0000

    Figuring out more of the parser

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use nom::Context::{
     Code,
 };
 use nom::Err::*;
-use nom::types::*;
 
 #[derive(Debug, PartialEq)]
 struct JailConfParam {
@@ -20,6 +19,51 @@ struct JailConf {
     name:   String,
     params: Vec<JailConfParam>,
 }
+
+// Parse a valueless boolean in the style of:
+//   - allow.mount;
+//   - persist;
+//   - etc;
+//
+// Other types of value will error.
+named!(
+    parse_bool_param_no_value<&[u8], &[u8]>,
+    do_parse!(
+        res: take_until_either!(" +=;\n") >> // Consume until an interesting char
+             not!(is_a!(" +=\n"))         >> // Ensure it's not a banned char
+             char!(';')                   >> // Consume terminating ;
+        (res)
+    )
+);
+
+// Parse a parameter with an associated value.
+//   - allow.mount = true;
+//   - allow.sysvipc="1";
+//   - ip4.addr = "127.0.1.1";
+//   - ip4.addr += "127.0.1.2";
+//
+// Other types of value will error.
+// This will choke if a value contained a quoted double quote, we should be
+// able to use escaped!() to help with this.
+named!(
+    parse_param_with_value<&[u8], (&[u8], &[u8])>,
+    do_parse!(
+        param: take_until_either!(" +=;\n") >>
+               not!(is_a!(";\n"))           >> // We don't want end of line yet
+               space0                       >> // Optional spaces
+               opt_res!(char!('+'))         >> // Optional +
+               char!('=')                   >> // = is mandatory
+               space0                       >> // Optional spaces
+        value: delimited!(
+                   opt_res!(tag!("\"")),        // Possible opening quote
+                   take_until_either!("\";\n"), // value
+                   opt_res!(tag!("\""))         // Possible closing quote
+               )                            >>
+               not!(is_a!("\n"))            >> // Ensure no new line yet
+               char!(';')                   >> // Terminating ;
+        (param, value)
+    )
+);
 
 // Parse the jail name
 named!(
@@ -55,7 +99,7 @@ named!(
 
 named!(
     parse_params<&[u8], Vec<(&[u8], &[u8])>>,
-    many_till!(parse_param, tag!("}"))
+    many0!(parse_param)
 );
 
 named!(
@@ -99,7 +143,7 @@ fn main() {
                 Failure(f) => {
                     eprintln!("{:?}", f);
                 }
-                Incomplete(n) => {
+                Incomplete(_n) => {
                     eprintln!("{:?}", e);
                 },
             }
@@ -116,4 +160,114 @@ fn main() {
     println!("A: {:?}", stra);
     println!("B: {:?}", strc);
     println!("{:?}", d);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Valueless boolean params
+    #[test]
+    fn test_parse_bool_param_no_value() {
+        let item = "allow.mount;".as_bytes();
+        let res = parse_bool_param_no_value(item);
+        let ok = Ok(("".as_bytes(), "allow.mount".as_bytes()));
+
+        assert_eq!(res, ok);
+    }
+
+    #[test]
+    fn test_parse_bool_param_no_value_trailing_newline() {
+        let item = "allow.mount;\n".as_bytes();
+        let res = parse_bool_param_no_value(item);
+        let ok = Ok(("\n".as_bytes(), "allow.mount".as_bytes()));
+
+        assert_eq!(res, ok);
+    }
+
+    #[test]
+    fn test_parse_bool_param_no_value_with_value_error() {
+        let item = "allow.mount = true;".as_bytes();
+        let res = parse_bool_param_no_value(item);
+
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn test_parse_bool_param_multiline_a() {
+        let item = "allow.mount;\npersist;".as_bytes();
+        let res = parse_bool_param_no_value(item);
+        let ok = Ok(("\npersist;".as_bytes(), "allow.mount".as_bytes()));
+
+        assert_eq!(res, ok);
+    }
+
+    #[test]
+    fn test_parse_bool_param_multiline_error() {
+        let item = "allow.mount\n;".as_bytes();
+        let res = parse_bool_param_no_value(item);
+
+        assert!(res.is_err());
+    }
+
+    // Parameters with values
+    #[test]
+    fn test_parse_param_with_value() {
+        let item = "allow.mount = true;".as_bytes();
+        let res = parse_param_with_value(item);
+        let ok = Ok((
+                "".as_bytes(),
+                ("allow.mount".as_bytes(), "true".as_bytes())
+                ));
+
+        assert_eq!(res, ok);
+    }
+
+    #[test]
+    fn test_parse_param_with_value_trailing_newline() {
+        let item = "allow.mount = true;\n".as_bytes();
+        let res = parse_param_with_value(item);
+        let ok = Ok((
+                "\n".as_bytes(),
+                ("allow.mount".as_bytes(), "true".as_bytes())
+                ));
+
+        assert_eq!(res, ok);
+    }
+
+    #[test]
+    fn test_parse_param_with_quoted_value() {
+        let item = "allow.mount = \"true\";".as_bytes();
+        let res = parse_param_with_value(item);
+        let ok = Ok((
+                "".as_bytes(),
+                ("allow.mount".as_bytes(), "true".as_bytes())
+                ));
+
+        assert_eq!(res, ok);
+    }
+
+    #[test]
+    fn test_parse_param_with_quoted_value_trailing_newline() {
+        let item = "allow.mount = \"true\";\n".as_bytes();
+        let res = parse_param_with_value(item);
+        let ok = Ok((
+                "\n".as_bytes(),
+                ("allow.mount".as_bytes(), "true".as_bytes())
+                ));
+
+        assert_eq!(res, ok);
+    }
+
+    #[test]
+    fn test_parse_param_with_quoted_value_no_spaces() {
+        let item = "allow.mount=\"true\";".as_bytes();
+        let res = parse_param_with_value(item);
+        let ok = Ok((
+                "".as_bytes(),
+                ("allow.mount".as_bytes(), "true".as_bytes())
+                ));
+
+        assert_eq!(res, ok);
+    }
 }
