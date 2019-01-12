0f071703087c58efc6529d764533478814cc0703
David O'Rourke <david.orourke@gmail.com>
2019-01-12 01:59:12 +0000

    JailBlock parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
 use std::fs::File;
 use std::io;
-use std::str;
 use nom::*;
 use nom::Context::{
     Code,
@@ -59,6 +58,7 @@ named!(
 // Other types of value will error.
 // This will choke if a value contained a quoted double quote, we should be
 // able to use escaped!() to help with this.
+// The + is actually important here and we need to capture it.
 named!(
     parse_param_with_value<CompleteStr, (CompleteStr, CompleteStr)>,
     do_parse!(
@@ -79,13 +79,31 @@ named!(
     )
 );
 
+// Attempt to parse a jail block.
+named!(
+    parse_block<CompleteStr, JailConf>,
+    do_parse!(
+        name:  ws!(take_until_either!(" {")) >> // Read the name
+               char!('{')                    >> // Mandatory opening {
+        block: ws!(parse_input)              >> // Recursive parsing. Oh no.
+        ws!(char!('}'))                      >> // Mandatory terminating }
+        (JailConf::Block(                       // JailBlock to return
+            JailBlock{
+                name:   name,
+                params: block,
+            }
+        ))
+    )
+);
 
+// Attempt to parse the given jail.conf input
 named!(
     parse_input<CompleteStr, Vec<JailConf>>,
     do_parse!(
         // We attempt parsers many times until the input is exhausted.
         config: many0!(
             // Config could be in pretty much any order.
+            // Surrounding whitespace will be trimmed.
             ws!(alt!(
                 // Parse a boolean parameter with no values.
                 parse_bool_param_no_value => { |param|
@@ -102,7 +120,6 @@ named!(
                 }
             ))
         ) >>
-
         (config)
     )
 );
@@ -247,6 +264,18 @@ mod tests {
     }
 
     #[test]
+    fn test_parse_param_with_quoted_emoji_value() {
+        let item = CompleteStr("smile.emoji = \"😊\";");
+        let res = parse_param_with_value(item);
+        let ok = Ok((
+                CompleteStr(""),
+                (CompleteStr("smile.emoji"), CompleteStr("😊"))
+                ));
+
+        assert_eq!(res, ok);
+    }
+
+    #[test]
     fn test_parse_param_with_quoted_value_trailing_newline() {
         let item = CompleteStr("allow.mount = \"true\";\n");
         let res = parse_param_with_value(item);
@@ -298,6 +327,125 @@ mod tests {
                 value: CompleteStr("/bin/sh /etc/rc.shutdown"),
             }),
         ];
+
+        let ok = Ok((CompleteStr(""), jc));
+
+        assert_eq!(res, ok);
+    }
+
+    #[test]
+    fn test_parse_block() {
+        let input = r#"
+            nginx {
+                    host.hostname = "nginx";
+                    path = "/usr/jails/nginx";
+                    ip4.addr += "lo1|127.0.1.1/32";
+                    ip6.addr += "lo1|fd00:0:0:1::1/64";
+                    ip4.addr += "em0|192.168.5.1/32";
+                    exec.start += "sleep  2 ";
+                    allow.raw_sockets = 0;
+                    exec.clean;
+                    exec.system_user = "root";
+                    exec.jail_user = "root";
+                    exec.start += "/bin/sh /etc/rc";
+                    exec.stop = "";
+                    exec.consolelog = "/var/log/jail_nginx_console.log";
+                    mount.fstab = "/etc/fstab.nginx";
+                    mount.devfs;
+                    mount.fdescfs;
+                    mount.procfs;
+                    allow.mount;
+                    allow.set_hostname = 0;
+                    allow.sysvipc = 0;
+                    enforce_statfs = "2";
+            }
+            "#;
+
+        let res = parse_block(input.into());
+        let jc = JailConf::Block(JailBlock{
+            name: CompleteStr("nginx"),
+            params: vec![
+                JailConf::ParamValue(JailParamValue{
+                    name:  "host.hostname".into(),
+                    value: "nginx".into(),
+                }),
+                JailConf::ParamValue(JailParamValue{
+                    name:  "path".into(),
+                    value: "/usr/jails/nginx".into(),
+                }),
+                JailConf::ParamValue(JailParamValue{
+                    name:  "ip4.addr".into(),
+                    value: "lo1|127.0.1.1/32".into(),
+                }),
+                JailConf::ParamValue(JailParamValue{
+                    name:  "ip6.addr".into(),
+                    value: "lo1|fd00:0:0:1::1/64".into(),
+                }),
+                JailConf::ParamValue(JailParamValue{
+                    name:  "ip4.addr".into(),
+                    value: "em0|192.168.5.1/32".into(),
+                }),
+                JailConf::ParamValue(JailParamValue{
+                    name:  "exec.start".into(),
+                    value: "sleep  2 ".into(),
+                }),
+                JailConf::ParamValue(JailParamValue{
+                    name:  "allow.raw_sockets".into(),
+                    value: "0".into(),
+                }),
+                JailConf::ParamBool(JailParamBool{
+                    name: "exec.clean".into(),
+                }),
+                JailConf::ParamValue(JailParamValue{
+                    name:  "exec.system_user".into(),
+                    value: "root".into(),
+                }),
+                JailConf::ParamValue(JailParamValue{
+                    name:  "exec.jail_user".into(),
+                    value: "root".into(),
+                }),
+                JailConf::ParamValue(JailParamValue{
+                    name:  "exec.start".into(),
+                    value: "/bin/sh /etc/rc".into(),
+                }),
+                JailConf::ParamValue(JailParamValue{
+                    name:  "exec.stop".into(),
+                    value: "".into(),
+                }),
+                JailConf::ParamValue(JailParamValue{
+                    name:  "exec.consolelog".into(),
+                    value: "/var/log/jail_nginx_console.log".into(),
+                }),
+                JailConf::ParamValue(JailParamValue{
+                    name:  "mount.fstab".into(),
+                    value: "/etc/fstab.nginx".into(),
+                }),
+                JailConf::ParamBool(JailParamBool{
+                    name: "mount.devfs".into(),
+                }),
+                JailConf::ParamBool(JailParamBool{
+                    name: "mount.fdescfs".into(),
+                }),
+                JailConf::ParamBool(JailParamBool{
+                    name: "mount.procfs".into(),
+                }),
+                JailConf::ParamBool(JailParamBool{
+                    name: "allow.mount".into(),
+                }),
+                JailConf::ParamValue(JailParamValue{
+                    name:  "allow.set_hostname".into(),
+                    value: "0".into(),
+                }),
+                JailConf::ParamValue(JailParamValue{
+                    name:  "allow.sysvipc".into(),
+                    value: "0".into(),
+                }),
+                JailConf::ParamValue(JailParamValue{
+                    name:  "enforce_statfs".into(),
+                    value: "2".into(),
+                }),
+            ],
+        });
 
         let ok = Ok((CompleteStr(""), jc));
 
